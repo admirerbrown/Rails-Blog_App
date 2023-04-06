7d60943ebb2f007df5d3a0c55f0f5e26b1e4973a
admirerbrown <admirerbrown@gmail.com>
2023-04-06 12:39:21 +0000

    clean code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,5 +6,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_posts = Post.where(author_id: @user.id)
+    @recent_posts = @user.recent_posts
   end
 end
