e3684b45993bd01a39042e9c0140e2b755492d19
admirerbrown <admirerbrown@gmail.com>
2023-04-13 12:43:23 +0000

    Add email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
+  attr_accessor :unconfirmed_email
+
   has_many :posts, foreign_key: :author_id
   has_many :likes, foreign_key: :author_id
   has_many :comments, foreign_key: :author_id
