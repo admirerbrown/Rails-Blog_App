fa7bc452bdee5ec4178e9fdf56a80679dee80705
Roy Jumah <royjumah17@gmail.com>
2023-04-14 12:14:59 +0300

    define roles for the admin and current user

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,1 +1,14 @@
 
+class Ability
+  include CanCan::Ability
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.is? :admin
+      can :manage, :all
+    else
+      can :read, :all
+      can %i[create destroy], Post, author_id: user.id
+      can %i[create destroy], Comment, user_id: user.id
+    end
+  end
+end
